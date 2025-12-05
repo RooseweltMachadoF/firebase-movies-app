f98cbc33e97ee37a63004eb150f7db2223c3f2f3
Roosewelt Machado <roosewelt.machadof@gmail.com>
2025-12-05 13:33:44 -0300

    Autenticacoes firebase
    instalacoes do CLI, FirabeseAuth e FirebaseCore

---
+++ firebase_movies_app/lib/core/services/firebase/firebase_auth/firebase_auth_service.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+
+class FirebaseAuthService {
+  static final FirebaseAuthService _singleton = FirebaseAuthService._internal();
+
+  factory FirebaseAuthService() {
+    return _singleton;
+  }
+  
+  FirebaseAuthService._internal();
+
+  static User? get getUser => FirebaseAuth.instance.currentUser;
+
+  static Stream<User?> getUserStream (BuildContext context) => FirebaseAuth.instance.authStateChanges();
+
+  static Future<String?>  login (String email, String password) async{
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (error ) {
+      return error.message;
+    }
+  }
+
+  static Future<(String? error , UserCredential?)> signUp(String email, String password) async{
+    try {
+      final UserCredential user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+      return (null, user);
+    } on FirebaseAuthException catch (error ) {
+      return (error.message, null);
+    }
+  }
+
+  static Future<String?> logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      return null;
+    } on FirebaseAuthException catch (error ) {
+      return error.message;
+    }
+  }
+}
--- firebase_movies_app/lib/main.dart
+++ firebase_movies_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_movies_app/core/config/firebase_movies_app.dart';
+import 'package:firebase_movies_app/core/services/firebase/firebase_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await FirebaseService.init();
+  await dotenv.load();
   runApp(const FirebaseMoviesApp());
 }
